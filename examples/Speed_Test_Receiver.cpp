024ce5a22619d64559233f2d67a7497b5dea738e
Russ Taylor <russ@reliasolve.com>
2024-03-04 17:04:38 -0500

    Adding test programs for sending and receiving at expected video rates from a number of cameras. Runs in parallel.

---
+++ examples/Speed_Test_Receiver.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2024: Arizona Board of Regents on Behalf of the University of Arizona
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <ASDP_Core_API.h>
+using namespace asdp;
+
+static void receiveDataThread(ReceiverUDP& receiveSocket, size_t bytesPerPacket, size_t totalPackets,
+  std::mutex& printMutex, std::atomic<bool> &broken)
+{
+  std::vector<uint8_t> buffer(bytesPerPacket);
+  unsigned packetsReceived = 0;
+  std::vector<char> copyBuffer(bytesPerPacket);
+
+  // Loop through and receive packets until we've gotten them all or an error occurs
+  while (packetsReceived < totalPackets) {
+    Status status = receiveSocket.ReceiveBuffer(buffer);
+    if (status != OKAY) {
+      std::cerr << "Error receiving data: " << ErrorMessage(status) << std::endl;
+      return;
+    }
+
+    // Process the received data (replace this with your processing logic).
+    // Here, we check the data and then copy it to an external buffer on the heap, which would be a
+    // pinned GPU memory buffer for the real code.
+    if (buffer[0] != (packetsReceived % 256)) {
+      std::lock_guard<std::mutex> lock(printMutex);
+      std::cerr << "Error: Expected " << (packetsReceived % 256) << " but got " << (int)buffer[0] << std::endl;
+      broken = true;
+      break;
+    }
+    memcpy(copyBuffer.data(), buffer.data(), bytesPerPacket);
+
+    // Increment the number of packets received
+    packetsReceived++;
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  int cameras = 25;
+  float fps = 60.0;
+  int secondsWorth = 10;
+  std::string IP = "localhost";
+  int port = 12000;
+
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if (arg == "--cameras") {
+      cameras = std::stoi(argv[++i]);
+    } else if (arg == "--fps") {
+      fps = std::stof(argv[++i]);
+    } else if (arg == "--secondsWorth") {
+      secondsWorth = std::stoi(argv[++i]);
+    } else if (arg == "--IP") {
+      IP = argv[++i];
+    } else if (arg == "--port") {
+      port = std::stoi(argv[++i]);
+    } else if (arg[0] == '-') {
+      std::cerr << "Unknown option: " << arg << std::endl;
+      return 1;
+    } else {
+      std::cerr << "Unknown argument: " << arg << std::endl;
+      return 1;
+    }
+  }
+
+  std::cout << "ASDP Speed Test Receiver" << std::endl;
+  std::cout << "Listens for data from the Speed_Test_Sender and checks for dropped packets" << std::endl;
+  std::cout << "Run this before running the sender." << std::endl;
+  std::cout << "Usage: Speed_Test_Receiver [--cameras <number>] [--fps <number>] [--secondsWorth <number>] [--IP <string>] [--port <number>]" << std::endl;
+  std::cout << "       It listens on the port specified and a number above it for each camera." << std::endl;
+  std::cout << "The parameters here must match those used by the sender." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Cameras: " << cameras << std::endl;
+  std::cout << "FPS: " << fps << std::endl;
+  std::cout << "Seconds worth of data: " << secondsWorth << std::endl;
+  std::cout << "Listening on IP:Port and following " << IP << ":" << port << std::endl;
+
+  // Compute the total number of packets to receive, where we send 342 packets per frame.
+  size_t packetsPerFrame = 342;
+  size_t totalPacketsPerCamera = static_cast<size_t>(fps * secondsWorth) * packetsPerFrame;
+
+  // We receive three lines of 1024 pixels of 2 bytes each.
+  size_t bytesPerPacket = 1024 * 2 * 3;
+
+  // Create the receive sockets.
+  std::vector<ReceiverUDP> receiveSockets;
+  for (unsigned i = 0; i < cameras; i++) {
+    receiveSockets.push_back(ReceiverUDP("localhost", port + i));
+    if (receiveSockets.back().GetConstructorStatus() != OKAY) {
+      std::cerr << "Error creating receive socket: " << receiveSockets.back().GetConstructorStatus() << std::endl;
+      return 2;
+    }
+  }
+
+  // Start the specified number of threads.
+  std::mutex printMutex;
+  std::atomic<bool> broken(false);
+  std::vector<std::thread> receivers;
+  for (unsigned i = 0; i < cameras; i++) {
+    std::thread receiver(receiveDataThread, std::ref(receiveSockets[i]), bytesPerPacket,
+      totalPacketsPerCamera, std::ref(printMutex), std::ref(broken));
+    receivers.push_back(std::move(receiver));
+  }
+
+  // Wait for the threads to finish.
+  for (unsigned i = 0; i < cameras; i++) {
+    receivers[i].join();
+  }
+
+  // Check for any errors
+  if (broken) {
+    std::cerr << "Error: Packets were dropped" << std::endl;
+    return 3;
+  } else {
+    std::cout << "Success" << std::endl;
+  }
+
+  // Clean up resources
+  receivers.clear();
+  receiveSockets.clear();
+
+  return 0;
+}
---
+++ examples/Speed_Test_Sender.cpp
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2024: Arizona Board of Regents on Behalf of the University of Arizona
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <ASDP_Core_API.h>
+using namespace asdp;
+
+static void sendDataThread(std::vector<SenderUDP> sendSockets, std::atomic<bool> &beginSending,
+  size_t bytesPerPacket, size_t totalPackets, double packetPeriod,
+  std::mutex &printMutex)
+{
+  // Get a vector of data to send
+  std::vector<uint8_t> imageData(bytesPerPacket);
+
+  // Determine the floating-point number of microseconds between packets
+  double packetPeriodMicroseconds = packetPeriod * 1e6;
+
+  // Wait for the trigger signal and then go
+  while (!beginSending) {}
+
+  // Start time is now on the steady clock
+  std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
+
+  // Loop through the packets and send each when it is time
+  for (int packetNum = 0; packetNum < totalPackets; ++packetNum) {
+    // Prepare image data (replace this with your image data source)
+    // For simplicity, we use a placeholder array here
+    uint8_t* data = imageData.data();
+    // Fill the first entry in the image data with the packet number mod 128
+    imageData[0] = (packetNum) % 256;
+
+    // Determine the time when the next packet should be sent by adding the
+    // number of packets sent times the period between packets to the start time
+    std::chrono::time_point<std::chrono::steady_clock> nextPacketTime =
+      startTime + std::chrono::microseconds(static_cast<uint64_t>(packetNum * packetPeriodMicroseconds));
+
+    // Wait until the next packet should be sent
+    do {
+      auto now = std::chrono::steady_clock::now();
+    } while (std::chrono::steady_clock::now() < nextPacketTime);
+
+    if (false) {
+      std::lock_guard<std::mutex> lock(printMutex);
+      std::cout << "Sent" << std::endl;
+    }
+
+    // Send the data
+    for (auto& sendSocket : sendSockets) {
+      Status status = sendSocket.Send(data, bytesPerPacket);
+      if (status != OKAY) {
+        std::cerr << "Error sending data: " << ErrorMessage(status) << std::endl;
+        return;
+      }
+    }
+  }
+
+  // Report how many packets per second were sent
+  auto now = std::chrono::steady_clock::now();
+  double seconds = std::chrono::duration_cast<std::chrono::duration<double>>(now - startTime).count();
+  std::lock_guard<std::mutex> lock(printMutex);
+  std::cout << "Sent " << totalPackets << " packets in " << seconds << " seconds, or "
+    << totalPackets / seconds << " packets per second to each of "
+    << sendSockets.size() << " cameras" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+  int cameras = 25;
+  int threads = 5;
+  float fps = 60.0;
+  float secondsWorth = 10;
+  std::string IP = "localhost";
+  int port = 12000;
+  std::string NICName;
+
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if (arg == "--cameras") {
+      cameras = std::stoi(argv[++i]);
+    } else if (arg == "--threads") {
+      threads = std::stoi(argv[++i]);
+    } else if (arg == "--fps") {
+      fps = std::stof(argv[++i]);
+    } else if (arg == "--secondsWorth") {
+      secondsWorth = std::stof(argv[++i]);
+    } else if (arg == "--IP") {
+      IP = argv[++i];
+    } else if (arg == "--port") {
+      port = std::stoi(argv[++i]);
+    } else if (arg == "--NIC") {
+      NICName = argv[++i];
+    } else if (arg[0] == '-') {
+      std::cerr << "Unknown option: " << arg << std::endl;
+      return 1;
+    } else {
+      std::cerr << "Unknown argument: " << arg << std::endl;
+      return 1;
+    }
+  }
+
+  if ((cameras / threads) * threads != cameras) {
+    std::cerr << "Threads must divide the number of cameras" << std::endl;
+    return 2;
+  }
+
+  std::cout << "ASDP Speed Test Sender" << std::endl;
+  std::cout << "Sends data to a Speed_Test_Receiver at the requested rate" << std::endl;
+  std::cout << "Run this after running the receiver." << std::endl;
+  std::cout << "Usage: Speed_Test_Sender [--cameras <number>] [--threads <number>] [--fps <number>] [--secondsWorth <number>] [--IP <string>] [--port <number>] [--NIC <string>]" << std::endl;
+  std::cout << "       It sends to the port specified and a number above it for each camera." << std::endl;
+  std::cout << "The parameters here must match those used by the receiver except for threads must evenly divide cameras." << std::endl;
+  std::cout << "NICName specifies the IP address on a NIC to be used for sending, default uses the system-selected port" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Cameras: " << cameras << std::endl;
+  std::cout << "Threads: " << threads << std::endl;
+  std::cout << "FPS: " << fps << std::endl;
+  std::cout << "Seconds worth of data: " << secondsWorth << std::endl;
+  std::cout << "Sending to IP:Port and following: " << IP << ":" << port << std::endl;
+
+  // Compute the total number of packets to send, where we send 342 packets per frame.
+  size_t packetsPerFrame = 342;
+  size_t totalPacketsPerCamera = static_cast<size_t>(fps * secondsWorth) * packetsPerFrame;
+
+  // We send three lines of 1024 pixels of 2 bytes each.
+  size_t bytesPerPacket = 1024 * 2 * 3;
+
+  // Create sockets for sending and receiving. There is a batch of them for each sending thread.
+  std::vector< std::vector<SenderUDP> > sendSockets;
+  size_t sendsPerThread = cameras / threads;
+  for (unsigned i = 0; i < threads; i++) {
+    std::vector<SenderUDP> mySendSockets;
+    for (unsigned j = 0; j < sendsPerThread; j++) {
+      mySendSockets.push_back(SenderUDP("localhost", port + j + i * sendsPerThread));
+      if (mySendSockets.back().GetConstructorStatus() != OKAY) {
+        std::cerr << "Error creating send socket: " << mySendSockets.back().GetConstructorStatus() << std::endl;
+        return 1;
+      }
+    }
+    sendSockets.push_back(mySendSockets);
+  }
+
+  // Start the specified number of threads and wait a second for them to get ready.
+  std::atomic<bool> beginSending(false);
+  std::mutex printMutex;
+  std::vector<std::thread> senders;
+  for (int i = 0; i < threads; ++i) {
+    SenderUDP sendSocket(IP, port + i, false, NICName);
+    senders.push_back(std::thread(sendDataThread, sendSockets[i], std::ref(beginSending),
+           bytesPerPacket, totalPacketsPerCamera, 1.0 / (packetsPerFrame * fps), std::ref(printMutex)));
+  }
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Start the threads and wait for them to all quit.
+  std::cout << std::endl << "Starting threads to send " << totalPacketsPerCamera
+    << " packets per camera" << std::endl;
+  beginSending = true;
+  for (int i = 0; i < threads; ++i) {
+    senders[i].join();
+  }
+
+  // Clean up resources
+  senders.clear();
+  sendSockets.clear();
+
+  return 0;
+}
