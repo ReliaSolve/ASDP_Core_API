50716a6d0d82e01c4367761816a880c241266d45
Russ Taylor <russ@reliasolve.com>
2024-08-04 22:50:03 +0000

    Adding two new network card names

--- network_tuning.sh
+++ network_tuning.sh
@@ -4,8 +4,11 @@
 # script is for the specific network cards installed on the ASDP servers.
 
 # Enable jumbo packets on each interface
+echo "Two of the following commands will fail, due to different names on different hosts"
 sudo ifconfig enp1s0f0np0 mtu 9000
 sudo ifconfig enp1s0f1np1 mtu 9000
+sudo ifconfig enp66s0f0np0 mtu 9000
+sudo ifconfig enp66s0f1np1 mtu 9000
 
 # Set the global receive buffer size larger
 sudo sysctl -w net.core.rmem_max=26214400
